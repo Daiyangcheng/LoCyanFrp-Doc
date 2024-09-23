7bf794adecc7e379af83f091afbe95f28d413262
夏沫花火zzz <ami@muska.zip>
2024-09-24 01:15:19 +0800

    ?

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,7 +23,7 @@ export default defineUserConfig({
       "/app/proxy-management",
       {
         text: 'API',
-        link: '/api',
+        link: '/api.html',
       },
       "/app/contact",
     ],
